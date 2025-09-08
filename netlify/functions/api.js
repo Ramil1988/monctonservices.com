1d7ee9875d6e912723277632d527f380e8bf5829
“Ramil <ramil.sharapov1988@gmail.com>
2025-09-08 17:14:48 -0300

    refactor Netlify function to mount existing Express app under the Netlify Functions base path

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,12 @@
 // Netlify Function wrapper for the existing Express app
 const serverless = require("serverless-http");
+const express = require("express");
 
 // Import the Express app from the server
 const app = require("../../server/server");
 
-module.exports.handler = serverless(app);
+// Mount the existing app under the Netlify Functions base path
+const server = express();
+server.use("/.netlify/functions/api", app);
 
+module.exports.handler = serverless(server);
